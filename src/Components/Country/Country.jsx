94e8efe9d97aeff6204c171c223e6aab4127c86d
JuwelShaikh <juwelshaikh04@gmail.com>
2024-02-26 18:05:08 +0600

    ....

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import './Country.css';
 
 const Country = ({country}) => {
     console.log(country)
     const {name, flags, capital} = country;
+    const [Visited, setVisited] = useState(false);
+    const handleVisited = () => {
+        setVisited(true)
+    }
+    const [notVisited, setNotVisited] = useState(false);
+    const handleNotVisited = () => {
+        setNotVisited(true)
+    }
     return (
         <div className='country'>
             <h3>Name: {name.common}</h3>
             <h4>Capital: {capital}</h4>
             <img src={flags.png} alt="" />
+            <button onClick={handleVisited}>Visited</button>
+            {Visited && 'Cool!'}
+            <button onClick={handleNotVisited}>Not Visited</button>
+            {notVisited && 'Wish you all the best!'}
         </div>
     );
 };
