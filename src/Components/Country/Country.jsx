6fb14987c382de132feeca26d9cae6f6ee1eba8b
JuwelShaikh <juwelshaikh04@gmail.com>
2024-02-26 16:50:07 +0600

    Style added

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -2,10 +2,12 @@ import './Country.css';
 
 const Country = ({country}) => {
     console.log(country)
-    const {name} = country;
+    const {name, flags, capital} = country;
     return (
-        <div>
+        <div className='country'>
             <h3>Name: {name.common}</h3>
+            <h4>Capital: {capital}</h4>
+            <img src={flags.png} alt="" />
         </div>
     );
 };
